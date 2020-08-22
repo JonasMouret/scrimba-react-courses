aa62ec2da6dc8796d4f61fcec8714edf180f92f7
jonasmouret <jonas@Jonas-PC.localdomain>
2020-08-22 19:58:50 +0200

    Init Commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer () {
+    return (
+        <div>
+            <h1>Good Bye ...</h1>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header () {
+    return (
+        <div>
+            <h1>My WebApp</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Picture</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
